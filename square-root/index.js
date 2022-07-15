296dd2adadd1557c59c92bb635f0957736a9efe9
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2022-07-15 12:50:46 -0700

    save small change

---
+++ square-root/index.js
@@ -0,0 +1,51 @@
+//grab elements from the DOM
+
+const number1 = document.getElementById("numberBox1");
+const number2 = document.getElementById("numberBox2");
+const number3 = document.getElementById("numberBox3");
+const number4 = document.getElementById("numberBox4");
+const number5 = document.getElementById("numberBox5");
+
+const goButton = document.getElementById("go-btn");
+const clearButton = document.getElementById("clear-btn");
+const result = document.getElementById("resultDiv");
+let renderResult = document.createElement("p");
+
+//get the square root of each the number the user enters.
+
+function getSquareRoot(numbers) {
+    result.innerHTML = "";
+    clearButton.classList.toggle("hidden");
+    goButton.classList.toggle("hidden");
+    for (let i = 0; i<numbers.length; i++) {
+        let renderResult = document.createElement("p");
+        
+        renderResult.innerHTML = `The Square Root of ${numbers[i]} is ${Math.sqrt(numbers[i])}`;
+        result.append(renderResult);
+    }
+}
+
+//create a way to clear the previous values.
+
+function clearArray(numbers) {
+     clearButton.classList.toggle("hidden");
+     goButton.classList.toggle("hidden");
+   
+    for (let i = numbers.length - 1; i > 0; i--) {
+        result.innerHTML = "";
+    } 
+}
+
+//make the 'go' button work
+goButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    return getSquareRoot([parseInt(number1.value), parseInt(number2.value), parseInt(number3.value), parseInt(number4.value), parseInt(number5.value)]);
+})
+
+//make the clear button work
+clearButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    return clearArray([parseInt(number1.value), parseInt(number2.value), parseInt(number3.value), parseInt(number4.value), parseInt(number5.value)]);
+})
+
+//small change
